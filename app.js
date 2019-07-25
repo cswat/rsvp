ba87b7b9fb5b62a644821673d2952b88a814f656
cswat <connor.swatling+gh@gmail.com>
2019-07-25 15:17:47 -0600

    2019.07.25 update
    Added filtering and edit capabilities

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const form = document.getElementById('registrar')
 const input = form.querySelector('input')
 const ul = document.getElementById('invitedList')
+const h2 = document.querySelector('h2')
+
+//create filter and add it to page
+const filterDiv = document.createElement('div')
+filterDiv.className = 'filter'
+const filterCbox = document.createElement('input')
+filterCbox.type = 'checkbox'
+const filterLabel = document.createElement('Label')
+filterLabel.textContent = "Hide those who haven't responded"
+document.querySelector('.main').insertBefore(filterDiv, ul)
+filterDiv.appendChild(filterCbox)
+filterDiv.appendChild(filterLabel)
 
 form.addEventListener('submit', (e) => {
     //stop page from refreshing after submit
@@ -8,8 +20,12 @@ form.addEventListener('submit', (e) => {
 
     //create list item and add it in
     let li = document.createElement('li')
-    li.innerText = input.value
     ul.appendChild(li)
+
+    //create text in span for list item and add it to list item
+    let span = document.createElement('span')
+    span.textContent = input.value
+    li.appendChild(span)
     
     //create label for checkbox and add it to list item
     const label = document.createElement('label')
@@ -22,6 +38,12 @@ form.addEventListener('submit', (e) => {
     checkbox.label = 'Select if attendee is coming'
     label.appendChild(checkbox)
 
+    //create edit button for list item and add it to list item
+    let editBtn = document.createElement('button')
+    editBtn.className = 'edit'
+    editBtn.innerText = 'Edit'
+    li.appendChild(editBtn)
+
     //create remove button for list item and add it to list item
     let removeBtn = document.createElement('button')
     removeBtn.className = 'delete'
@@ -32,6 +54,7 @@ form.addEventListener('submit', (e) => {
     form.reset()
 });
 
+//handle checkbox for attending individuals
 ul.addEventListener('change', (e) => {
     const checkbox = e.target
     const checked = checkbox.checked
@@ -43,10 +66,49 @@ ul.addEventListener('change', (e) => {
     }
 });
 
+//handle button clicks
 ul.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
-      const li = e.target.parentNode
-      const ul = li.parentNode
-      ul.removeChild(li)
-    } 
-})
+        const button = e.target
+        const li = button.parentNode
+        const ul = li.parentNode
+        if ( button.textContent === 'Remove') {
+            ul.removeChild(li)
+        } else if (button.textContent === 'Edit') {
+            const span = li.firstElementChild
+            const input = document.createElement('input')
+            input.type = 'text'
+            input.value = span.textContent
+            li.insertBefore(input, span)
+            li.removeChild(span)
+            button.textContent = 'Save'
+        } else if (button.textContent === 'Save') {
+            const input = li.firstElementChild
+            const span = document.createElement('span')
+            span.textContent = input.value
+            li.insertBefore(span, input)
+            li.removeChild(input)
+            button.textContent = 'Edit'
+        }
+    }      
+});
+
+//handle filters
+filterDiv.addEventListener('change', (e) => {
+    const checkbox = e.target;
+    const checked = checkbox.checked;
+    const guestList = ul.children;
+    if (checked) {
+        for (i = 0; i < guestList.length; i++) {
+            if (guestList[i].className === 'responded') {
+                guestList[i].style.display = '';
+            } else {
+                guestList[i].style.display = 'none';
+            }
+        }
+    } else {
+        for (i = 0; i < guestList.length; i++) {
+            guestList[i].style.display = '';
+        }
+    }
+});
